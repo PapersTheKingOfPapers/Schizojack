d7636d51758c3cfbaf5ab557265773a46f41afb8
Stru66ler <116081614+Stru66ler@users.noreply.github.com>
2026-05-04 13:23:15 +0300

    Statsmonitorworsk

---
+++ Schizojack/Assets/Scripts/ButtonTesting.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ButtonTesting : MonoBehaviour
+{
+    public void button1()
+    {
+        Debug.Log("Button1Pressed");
+    }
+    public void button2()
+    {
+        Debug.Log("Button1Pressed");
+    }
+    public void button3()
+    {
+        Debug.Log("Button1Pressed");
+    }
+}
---
+++ Schizojack/Assets/Scripts/StatsMonitor.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StatsMonitor : MonoBehaviour
+{
+    [SerializeField] private GameObject statsUI;
+    [SerializeField] private Button statsButton;
+    [SerializeField] private GameObject optionsUI;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private GameObject areYouSureUI;
+
+    private enum menuStates
+    {
+        stats,
+        options,
+        areYouSure
+    }
+    private menuStates menuState;
+    private void Awake()
+    {
+        menuState = menuStates.stats;
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        //The fucking heartrate or sumshit in here.
+    }
+
+    //Buttons//
+    public void stats() //Stats Menu
+    {
+        menuState = menuStates.stats;
+        UpdateUI();
+    }
+    public void options() //Options Menu
+    {
+        menuState = menuStates.options;
+        UpdateUI();
+    }
+    public void back() //Back
+    {
+        menuState = menuStates.options;
+        UpdateUI();
+    }
+    public void quit()
+    {
+        menuState = menuStates.areYouSure;
+        UpdateUI();
+    }
+    public void confirmQuit() //Suicide
+    {
+        //Suicide code here When pressed player dies.
+    }
+
+    private void UpdateUI()
+    {
+        statsUI.SetActive(false);
+        optionsUI.SetActive(false);
+        areYouSureUI.SetActive(false);
+
+        statsButton.interactable = true;
+        optionsButton.interactable = true;
+
+        switch (menuState)
+        {
+            case menuStates.stats:
+                statsUI.SetActive(true);
+                statsButton.interactable = false;
+                break;
+            case menuStates.options:
+                optionsUI.SetActive(true);
+                optionsButton.interactable = false;
+                break;
+            case menuStates.areYouSure:
+                areYouSureUI.SetActive(true);
+                break;
+        }
+    }
+}
